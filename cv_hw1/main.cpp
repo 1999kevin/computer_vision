b8fe3c5a78ab489cd58cb307045277c5cb5e1d23
1999kevin <3160104016@zju.edu.cn>
2020-12-01 22:20:47 +0800

    first frame and drawline is done

--- cv_hw1/main.cpp
+++ cv_hw1/main.cpp
@@ -4,83 +4,130 @@
 using namespace std;
 using namespace cv;
 
+#define W 1080
+#define H 720
 
-char path[100];//输入文件路径 
+const char *path = "C:/Users/a/Desktop/courses/computer_vision/cv_hw1/test1.avi";//输入文件路径 
+Mat image;
+VideoWriter writer;
 
-
-void init(string filename)
+/* 写入一帧 */
+void putPicture(Mat img)
 {
-    //image= Mat::zeros(W, W, CV_8UC3);//创建一张空图像
-    image = Mat(H, W, CV_8UC3);
-    temp = Mat(H, W, CV_8UC3);
-    strcpy(path, filename);
-    writer = VideoWriter(path, CV_FOURCC('X', 'V', 'I', 'D'), 30, Size(W, H));
+    writer.write(Mat(img));
+}
+
+/* 延迟k帧 */
+void delay(int k)
+{
+    for (int i = 0; i <= k; i++)
+    {
+        putPicture(image);
+    }
+}
+
+
+void drawSubPhoto(const char *path, int loc_w, int loc_h)
+{
+    Mat roi = imread(path, IMREAD_COLOR);
+    Rect roi_rect = Rect(loc_w, loc_h, roi.cols, roi.rows);
+    roi.copyTo(image(roi_rect));
+}
+
+void drawFirstFrame()
+{
+    String name = "name    :   Liuke";
+    String number = "Student ID: 12021232";
+
+    putText(image, number, Point(W / 3, H/3), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 0, 0));
+    putText(image, name, Point(W/3, H/3*2), FONT_HERSHEY_COMPLEX, 1, Scalar(0, 0, 255));
+
+    drawSubPhoto("zheda(1).jpg", 128, 64);
 }
 
 
 
-void play()
+/* 清空图片，true则写入一帧，false不写入 */
+void drawBackground(bool flag)
 {
-    VideoCapture capture(path);
-    if (!capture.isOpened())
-        cout << "fail to open!" << endl;
-    ////获取整个帧数
-    //long totalFrameNumber = capture.get(CV_CAP_PROP_FRAME_COUNT);
-    //cout << "整个视频共" << totalFrameNumber << "帧" << endl;
-
-    //设置开始帧()
-    long frameToStart = 0;
-    capture.set(CV_CAP_PROP_POS_FRAMES, frameToStart);
-    cout << "从第" << frameToStart << "帧开始读" << endl;
-
-    //获取帧率
-    double rate = capture.get(CV_CAP_PROP_FPS);
-    cout << "帧率为:" << rate << endl;
-    //承载每一帧的图像
-    Mat frame;
-    //显示每一帧的窗口
-    namedWindow("Viedo");
-    //两帧间的间隔时间:
-    int delay = 1000 / rate;
-    long currentFrame = frameToStart;
-
-    while (1)
+    rectangle(image,
+        Point(0, 0),
+        Point(W, H),
+        Scalar(255, 255, 255),
+        -1,
+        8);
+    if (flag == true)
     {
-        //读取下一帧
-        if (!capture.read(frame))
-        {
-            break;
-            return;
-        }
-        //这里加滤波程序
-        imshow("Extracted frame", frame);
-        int c = waitKey(delay);
-        //按下按键后会停留在当前帧，等待下一次按键
-        if (c >= 0)
-        {
-            waitKey(0);
-        }
-        currentFrame++;
+        putPicture(image);
     }
-    //关闭视频文件
-    capture.release();
+}
+
+void drawLine(Mat mat, Point start, Point end,Scalar color,int thick, int rate)
+{
+    int x1, y1,step;
+    Point center=start;
+    x1 = start.x;
+    y1 = start.y;
+    double footx,footy;
+    int dx = end.x - start.x;
+    int dy = end.y - start.y;
+    circle(mat, center,thick,color,-1, 8, 0);
+    putPicture(mat);
+    if (abs(dx) > abs(dy))
+    {
+        step = abs(dx);
+    }
+    else 
+    {
+        step = abs(dy);
+    }
+    footx = (double)dx / step;
+    footy = (double)dy / step;
+    //printf("footy = %f\n", footy);
+    for (int i = 0; i<step; i++)
+    {
+        if (footx > 0) {
+            x1 += int(footx + 0.5);
+        }
+        if (footx < 0)
+        {
+            x1 += int(footx - 0.5);
+        }
+        if (footy > 0)
+        {
+            y1 += int(footy + 0.5);
+        }
+        if (footy < 0)
+        {
+            y1 += int(footy - 0.5);
+        }
+        center.x = x1;
+        center.y = y1;
+        circle(mat, center,thick,color,-1, 8, 0);
+        if(i % rate == 0){
+            putPicture(mat);
+        }
+            
+    }
+}
+
+void drawCartoon(){
+    drawLine(image, Point(0,0), Point(512,512), Scalar(255, 0, 0), 1, 5);
 }
 
 
 int main()
 {
-    printf("正在生成视频，请稍后");
-    init("C:/Users/a/Desktop/course/computer_vision/cv_hw1/test.avi");
-    // drawBackground(true);
-    // drawText();
-    // drawBear();
-    // drawRh();
-    // drawStar(image, Point(0, 300), 40, Scalar(255, 255, 0), 2);
-    // drawStar(image, Point(300, 300), 50, Scalar(255, 0, 255), 2);
-    // drawStar(image, Point(0, 0), 35, Scalar(255, 0, 0), 2);
-    // drawStar(image, Point(-350, 250), 35, Scalar(255, 0, 255), 2);
-    imshow("pig", image);
-    // play();
-    waitKey();
+    printf("making up video\n");
+    writer = VideoWriter(path, 0, 30, Size(W, H));
+    image= Mat::zeros(H, W, CV_8UC3);
+    drawBackground(false);
+    delay(30);
+    drawFirstFrame();
+    delay(30);
+    drawBackground(false);
+    drawCartoon();
     return 0;
 }
+
+
