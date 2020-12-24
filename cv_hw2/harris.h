9106da58b763f703def292f10099ae3682bb8864
keliu <3160104016@zju.edu.cn>
2020-12-24 11:19:39 +0800

    code function done

--- cv_hw2/harris.h
+++ cv_hw2/harris.h
@@ -1,11 +1,66 @@
 #ifndef _HARRIS_H_
 #define _HARRIS_H_
-// #include <opencv2/cosre/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/shape/shape.hpp>
 #include <vector>
 
-// using namespace cv;
+#include <iostream>
 
-void update(void);
+using namespace cv;
+using namespace std;
+
+class Harris
+{
+private:
+    const float k = 0.05; //计算R时用到的参数    /* 这个真的可以用int吗 */
+
+    Mat Ix2;     //x方向的梯度(一阶导)
+    Mat Iy2;     //y方向的梯度(一阶导)
+    Mat Ixy;    //Ix * Iy
+
+    Mat R;      //R矩阵
+    Mat mmax;
+    Mat mmin;
+
+    int max_r;
+
+    int image_w;
+    int image_h;
+
+    Mat calIx(Mat image);
+    Mat calIy(Mat image);
+    Mat calIxy(cv::Mat Ix,cv::Mat Iy);
+
+    template<typename TYPE>
+    void absMat(cv::Mat& mat);
+
+    template<typename TYPE>
+    Mat powMat(cv::Mat& mat,int n=2);
+
+    void meanFilter(cv::Mat& image);
+
+
+
+    void calR();
+
+    template<typename IMGTYPE,typename FILTERTYPE>
+    cv::Mat convolve(cv::Mat image,cv::Mat filter);
+public:
+    Harris();
+    ~Harris();
+    void update(Mat image);
+
+    vector<cv::Point> getCorners(int threshold=0);
+    void getEigenvalue();
+    cv::Mat getR();
+    cv::Mat getMax();
+    cv::Mat getMin();
+
+};
 
 
 #endif
--- cv_hw2/main.cpp
+++ cv_hw2/main.cpp
@@ -5,18 +5,47 @@
  * @function: Harris角点检测器
 */
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/shape/shape.hpp>
-
 #include "harris.h"
 using namespace cv;
 using namespace std;
+
+void cut(Mat image){
+    Harris harris;
+    Mat min;
+    Mat max;
+    Mat R;
+
+    Mat result = image.clone();
+    harris.update(image);
+    // printf("one cut\n");
+    vector<Point> corners = harris.getCorners();
+    int size = corners.size();
+    for(int i=0;i<size;i++){
+        int x = corners[i].x;
+        int y = corners[i].y;
+        rectangle(result,cv::Point(x-4,y-4),cv::Point(x+4,y+4),cv::Scalar(0,255,0),2);
+    }
+
+    min = harris.getMin();
+    max = harris.getMax();
+    R   = harris.getR();
+
+    //保存图片
+    imwrite("result.jpg",result);
+    imwrite("min.jpg",min);
+    imwrite("max.jpg",max);
+    imwrite("R.jpg",R);
+
+    //展示中间结果
+    imshow("result",result);
+    imshow("min",min);
+    imshow("max",max);
+    imshow("R",R);
+}
+
+
 int main(int argc, char* argv[])
 {
-    update();
-
     VideoCapture cap(0);
     if(!cap.isOpened()){
         cout<<"error: can't open the camera"<<endl;
@@ -26,26 +55,21 @@ int main(int argc, char* argv[])
     Mat frame;
     namedWindow("frame",1);
 
-
     while(true){
         cap>>frame;
         imshow("frame",frame);
-        // char key = waitKey(3);
-        char c = waitKey(20);    //延时30
-        if(c == 'q')
-        {
-            break;
+        char key = waitKey(30);    //延时30
+        
+        if(key == ' '){
+            cut(frame);
+            char key2 = waitKey(30);   //这个延时后期可以删掉
+            while(key2 != ' '){
+                key2 = waitKey(30);
+                if(key2 == 27) return 1;
+            }
+        }else if(key == 27){ //esc键退出
+            return 1;
         }
-        // if(key == ' '){     //
-        //     //  cut(frame);
-        //      char key2; //阻塞暂停
-        //      do{
-        //          key2 = waitKey();
-        //          if(key2 == 27) return 1;
-        //      }while(key2 != ' ');
-        // }else if(key == 27){ //esc键退出
-        //     return 1;
-        // }
     }
     return 0;
 }
